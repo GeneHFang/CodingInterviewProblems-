3fc29cfd5ce6477ffa86b4d82faebabf92e54baf
GeneHFang <gene.li@nyu.edu>
2020-05-10 19:22:54 -0400

    added insertion sort function to aid in bucket sort demo

---
+++ Javascript/sortFunctions/sortFunctions.js
@@ -0,0 +1,28 @@
+
+/**
+ * Classic insertion sort in place. 
+ * @args Array of numbers, boolean indicating ascending (true) or descending(false)
+ * @returns null, modifies argument array.
+ */
+export const insertionSort = (array, ascending) => {
+    let n = array.length;
+    for(let i = 0; i < n ; i++){
+        let key = array[i];
+        let j = i - 1;
+        
+        if (ascending){
+            while (j >= 0 && array[j] > key){
+                array[j+1] = array[j];
+                j = j-1;
+            }
+        }
+        else {
+            while (j >= 0 && array[j] < key){
+                array[j+1] = array[j];
+                j = j-1;
+            }
+        }
+
+        array[j+1] = key; 
+    }
+}
