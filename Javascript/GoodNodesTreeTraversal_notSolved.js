f95c963209919665723af5c707e129fd3449e723
GeneHFang <gene.li@nyu.edu>
2020-07-12 19:32:20 -0400

    attempt #2 after review of traversals

--- Javascript/GoodNodesTreeTraversal_notSolved.js
+++ Javascript/GoodNodesTreeTraversal_notSolved.js
@@ -1,57 +1,47 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * Trees are represented as an Array
+ *  [1, 2, 3, 4, null, 6, 7] 
+ *  is
+ *    1
+ *  2   3
+ * 4   6 7
+ * 
  */
 /**
- * @param {TreeNode} root
+ * @param {Array} root
  * @return {number}
  */
 var goodNodes = function(root) {//If empty tree even though question specifies nodes expected [1,10^5]
     if (root.length <= 0) return 0;
     else if (root.length === 1) return 1;
     
-    //Root is always good
-    let numGoodHash = {
-        numGood: 1,
+    let numGood = 0
+    let iterator = 0; 
+    let max = root[iterator]; 
+    
+    recursivePreOrder(root, max, iterator);
+    
+    return numGood;
+    
+    function recursivePreOrder(root, max, iterator) {
+        let rownum = Math.floor(Math.log2(iterator+1)) + 1; 
+        let left = rownum*iterator + 1;
+        let right = rownum*iterator + 2;
+
+        if (root[iterator] >= max) {
+            numGood++;
+            max = root[iterator];
+        }
+        if (root[left]){
+            recursivePreOrder(root, max, left);
+        }
+        if (root[right]){
+            recursivePreOrder(root, max, right);
+        }
     };
-     
-    let iterator = 0;
-    let rownum = 1; 
-    
-    recursivePreOrder(root, numGoodHash, iterator, rownum);
-    
-    return numGoodHash.numGood;
-    
+
     
 };
 
 
-const recursivePreOrder = (root, hash, iterator, rownum) => {
-    let left = rownum*iterator + 1;
-    let right = rownum*iterator + 2;
-    if (root[left]===undefined && root[right]===undefined) {
-        return;
-    }
-    else if (root[left]){
-        if (root[left] >= root[iterator]) hash.numGood++;
-       
-        
-        iterator = left; 
-        rownum++;
-        recursivePreOrder(root, hash, iterator, rownum);
-    }
-    else if (root[right]){
-        if (root[right] >= root[iterator]) hash.numGood++;
-        
-        iterator = right;
-        rownum++;
-        recursivePreorder(root, hash, iterator, rownum);
-    }
-    else {
-        console.log("shouldn't be here")
-    }
-};
+
