c91b4c7833c0558b6983994f24b5e4ef5e6971a8
GeneHFang <gene.li@nyu.edu>
2020-07-12 18:56:34 -0400

    attempt #1

--- Javascript/GoodNodesTreeTraversal_notSolved.js
+++ Javascript/GoodNodesTreeTraversal_notSolved.js
@@ -10,6 +10,48 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var goodNodes = function(root) {
-    //solution here   
-}
+var goodNodes = function(root) {//If empty tree even though question specifies nodes expected [1,10^5]
+    if (root.length <= 0) return 0;
+    else if (root.length === 1) return 1;
+    
+    //Root is always good
+    let numGoodHash = {
+        numGood: 1,
+    };
+     
+    let iterator = 0;
+    let rownum = 1; 
+    
+    recursivePreOrder(root, numGoodHash, iterator, rownum);
+    
+    return numGoodHash.numGood;
+    
+    
+};
+
+
+const recursivePreOrder = (root, hash, iterator, rownum) => {
+    let left = rownum*iterator + 1;
+    let right = rownum*iterator + 2;
+    if (root[left]===undefined && root[right]===undefined) {
+        return;
+    }
+    else if (root[left]){
+        if (root[left] >= root[iterator]) hash.numGood++;
+       
+        
+        iterator = left; 
+        rownum++;
+        recursivePreOrder(root, hash, iterator, rownum);
+    }
+    else if (root[right]){
+        if (root[right] >= root[iterator]) hash.numGood++;
+        
+        iterator = right;
+        rownum++;
+        recursivePreorder(root, hash, iterator, rownum);
+    }
+    else {
+        console.log("shouldn't be here")
+    }
+};
